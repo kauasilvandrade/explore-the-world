8624132564986a219c5140b69e833351862bb3f8
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2023-02-15 14:30:00 -0300

    terminei a acessibilidade do menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+
 const btnMenu = document.querySelector(".cabecalho__btnMenu");
 
 // FUNÇÃO PARA ABRIR O MENU
-function menuShow() {
+function menuShow(event) {
 
+    // MENU
     const listaMenu = document.querySelector(".cabecalho__listaMenu");
     listaMenu.classList.toggle("ativado");
 
@@ -12,6 +14,19 @@ function menuShow() {
     const flechaMenu = document.querySelector(".hamburguer");
     flechaMenu.classList.toggle("ativado");
 
+    // ACESSIBILIDADE
+    if (event.type == "touchstart") event.preventDefault();
+
+    const menuAtivado = listaMenu.classList.contains("ativado");
+    
+    event.currentTarget.setAttribute("aria-expanded", menuAtivado);
+
+    if (menuAtivado) {
+        event.currentTarget.setAttribute("aria-label", "Botão fechar menu")
+    } else {
+        event.currentTarget.setAttribute("aria-label", "Botão abrir menu")
+    }
 }
 
 btnMenu.addEventListener("click", menuShow);
+btnMenu.addEventListener("touchstart", menuShow);
